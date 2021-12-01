8a781c95143e7ea3684ab837a7b5f572b1522160
Paulo Vinícius Oliveira Santos <73082656+paulovinicius1973@users.noreply.github.com>
2021-11-30 23:39:07 -0300

    Update provider.tf

--- Hands-on AWS - Projeto Final/terraform/provider.tf
+++ Hands-on AWS - Projeto Final/terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIAZ6KAEGC3GXILS4GY"
-    secret_key = "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"
+    access_key = "A"
+    secret_key = "W"
 }
